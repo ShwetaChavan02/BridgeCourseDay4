ed577296d50c60760ad5f9aa58fe03693c769161
ShwetaChavan02 <shwetachavan0204@gmail.com>
2026-09-17 14:58:53 +0530

    factorial using recursion

---
+++ code9.py
@@ -0,0 +1,9 @@
+#factorial using recursion
+
+#n=int(input("enter number:"))
+def factorial(n):
+    if (n == 0):
+        return 1
+    else:
+        return n * factorial(n-1)
+    print(factorial(n))
